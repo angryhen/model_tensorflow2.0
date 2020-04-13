f85d17245f4837f7b202fef2ec9500d6068e281d
du <982830737@qq.com>
2020-04-13 19:03:22 +0800

    update config

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 src_dataset = '/home/du/Desktop/my_project/Basic_CNNs_TensorFlow2/dataset'
 dst_datadir = 'tfrecord/'
 train_tfrecord = dst_datadir + 'train.tfrecord'
-valid_tfrecord= dst_datadir + 'valid_tfrecord'
-test_tfrecord = dst_datadir + 'test_tfrecord'
+valid_tfrecord= dst_datadir + 'valid.tfrecord'
+test_tfrecord = dst_datadir + 'test.tfrecord'
 
 size = 224
